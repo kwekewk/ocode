27686170613184e30ccb394bb8fad12318e08be2
kwekewk <fsetiawanputra@gmail.com>
2023-07-14 20:17:52 +0700

    one-api during building

--- on_startup.sh
+++ on_startup.sh
@@ -5,3 +5,20 @@
 # cd transformers && pip install -e ".[dev]"
 # useradd -m aku && adduser aku sudo && echo 'aku:aku' | sudo chpasswd && sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 # /usr/bin/newuidmap 1500 0 1000 1 1 100000 65536
+git clone https://github.com/ckt1031/one-api/
+
+# Build the frontend
+cd one-api/web
+npm install
+npm run build
+
+# Build the backend
+cd ..
+export GO111MODULE=on
+export CGO_ENABLED=1
+export GOOS=linux
+python ./i18n/translate.py --repository_path . --json_file_path ./i18n/en.json
+go mod download
+go build -ldflags "-s -w -X 'one-api/common.Version=$(cat VERSION)' -extldflags '-static'" -o one-api
+chmod u+x one-api
+#./one-api --port 3000 --log-dir ./logs
--- start_server.sh
+++ start_server.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+service nginx start
+
 echo "Starting VSCode Server..."
 /app/openvscode-server/bin/openvscode-server --host 0.0.0.0 --port 7860 --without-connection-token "${@}" &
 
-echo "Starting Code Tunnel..."
-/usr/bin/code tunnel --accept-server-license-terms &
+/home/user/one-api/one-api --port 3000 --log-dir ./logs &
+
+#echo "Starting Code Tunnel..."
+#/usr/bin/code tunnel --accept-server-license-terms &
 
 # Sleep for a long time to keep the container running
 sleep infinity
