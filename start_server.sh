66dc91f2dbe8686d8e830c6c2b3bab68c3b3d528
nyanya <nyanya@.nya.con>
2023-07-24 11:17:43 +0700

    Revert "fix startser"
    This reverts commit c8b2a1795922b6f74496054d1d4f70ca84b60b2e.

--- start_server.sh
+++ start_server.sh
@@ -3,11 +3,11 @@
 service nginx start
 
 echo "Starting VSCode Server..."
-#/app/openvscode-server/bin/openvscode-server --host 0.0.0.0 --port 7860 --without-connection-token "${@}" &
+/app/openvscode-server/bin/openvscode-server --host 0.0.0.0 --port 7860 --without-connection-token "${@}" &
 
 #/home/user/one-api/one-api --port 3000 --log-dir ./logs &
 
-/usr/bin/code-server --bind-addr 0.0.0.0:7860 --auth password --user-data-$ &
+/usr/bin/code-server --bind-addr 0.0.0.0:8080 --auth password --user-data-$ &
 
 #echo "Starting Code Tunnel..."
 #/usr/bin/code tunnel --accept-server-license-terms &
