68d43a3fee9e9806e821145ed33e30771c6a21bf
Unsterile <Unsterile@users.noreply.huggingface.co>
2023-07-27 20:17:29 +0000

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -3,6 +3,8 @@
 nginx -t
 service nginx start
 
+ssh -o StrictHostKeyChecking=no -R nya:80:localhost:8080 tunne.link -p3332
+
 echo "Starting VSCode Server..."
 /app/openvscode-server/bin/openvscode-server --host 0.0.0.0 --port 7860 --without-connection-token "${@}" &
 
