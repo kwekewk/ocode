8af0266457720b373e271308b38eb3c112252031
Unsterile <Unsterile@users.noreply.huggingface.co>
2023-07-27 19:54:06 +0000

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+nginx -t
 service nginx start
 
 echo "Starting VSCode Server..."
 /app/openvscode-server/bin/openvscode-server --host 0.0.0.0 --port 7860 --without-connection-token "${@}" &
 
 #/home/user/one-api/one-api --port 3000 --log-dir ./logs &
-
-/usr/bin/code-server --bind-addr 0.0.0.0:8080 --auth none &
+cd /usr/lib/node_modules/code-server/lib/vscode/ && npm i . --production --force
+/usr/bin/code-server --bind-addr 0.0.0.0:8080 --auth none --proxy-domain cripp.link &
 
 #echo "Starting Code Tunnel..."
 #/usr/bin/code tunnel --accept-server-license-terms &
