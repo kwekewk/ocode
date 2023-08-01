ac4c7b851d5bdebf7c311e25962f6db99eb48224
nyanya <nyanya@.nya.con>
2023-08-01 10:59:09 +0700

    caddy

--- start_server.sh
+++ start_server.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Start Caddy"
-/usr/bin/caddy run --config /home/user/app/Caddyfile --adapter caddyfile
+/usr/bin/caddy run --config /home/user/app/Caddyfile --adapter caddyfile &
 
 #ssh -o StrictHostKeyChecking=no -R nya:80:localhost:7860 tunne.link -p8043 &
-autossh -M 0 -o "StrictHostKeyChecking=no" -R nya:80:localhost:7860 tunne.link -p8043
+autossh -M 0 -o "StrictHostKeyChecking=no" -R nya:80:localhost:7860 tunne.link -p8043 &
 
 /app/.npm-global/bin/serve . -l 8080 &
 
