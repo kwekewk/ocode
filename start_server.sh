b1b613c6d3cd2480b1a753b17ae83f650d3f101f
nyanya <nyanya@.nya.con>
2023-08-01 09:51:34 +0700

    fix

--- start_server.sh
+++ start_server.sh
@@ -2,5 +2,7 @@
 
 ssh -o StrictHostKeyChecking=no -R nya:80:localhost:7860 tunne.link -p8043 &
 
+serve . -l 8080 &
+
 echo "Starting VSCode Server..."
 /app/openvscode-server/bin/openvscode-server --host 0.0.0.0 --port 7860 --without-connection-token "${@}" 
