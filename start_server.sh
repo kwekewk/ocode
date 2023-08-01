adc5d492dbfe89f051bbdc3a6459ef615f067ed4
nyanya <nyanya@.nya.con>
2023-08-01 12:35:18 +0700

    caddy handle

--- start_server.sh
+++ start_server.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "Start Caddy"
-/usr/bin/caddy run --config /home/user/app/Caddyfile --adapter caddyfile &
-
 #ssh -o StrictHostKeyChecking=no -R nya:80:localhost:7860 tunne.link -p8043 &
 autossh -M 0 -o "StrictHostKeyChecking=no" -R nya:80:localhost:7860 tunne.link -p8043 &
 
 /app/.npm-global/bin/serve . -l 8080 &
 
 echo "Starting VSCode Server..."
-/app/openvscode-server/bin/openvscode-server --host 0.0.0.0 --port 7860 --without-connection-token "${@}" 
+/app/openvscode-server/bin/openvscode-server --host 0.0.0.0 --port 7860 --without-connection-token "${@}" &
+
+echo "Start Caddy"
+/usr/bin/caddy run --config /home/user/app/Caddyfile --adapter caddyfile
